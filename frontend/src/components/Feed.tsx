a1af9bcd95fa9594b11be3043ed95cb9b4a9d9a4
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-04-18 22:13:32 +0200

    Frontend - Add matchs button.

--- frontend/src/components/Feed.tsx
+++ frontend/src/components/Feed.tsx
@@ -6,7 +6,7 @@ const Feed = () => {
     const {user} = useAuth();
 
   return (
-    <section className="w-full flex flex-col items-center gap-7">
+    <section className="w-full flex flex-col items-center gap-3">
         <h1 className="head_text text-center">
             Vote & Discuss about
             <br className="max-md:hidden"/> {/* from 0 -> md : hidden */}
@@ -19,14 +19,24 @@ const Feed = () => {
         <div className="flex flex-col justify-center items-center">
             <p
                 className="text-center text-slate-400 text-2xl"
-                >Don't waste your time and come enjoy with us..</p>
-            <div className='mx-6 my-2'>
+                >Don't waste your time and come enjoy with us..
+            </p>
+            {user && (
+                <div className='text-black bg-slate-400 hover:bg-slate-300 font-bold py-4 px-6 my-2 rounded' >
+                    <button>
+                        <Link to='/matchs'>
+                            Go to Matchs
+                        </Link>
+                    </button>
+                </div>
+            )}
+            <div className='mx-6'>
                 {user ? (
                     <div className="desc text-center">
                         Our amazing plateform is 100% free, No credit card required.
                     </div>  
                 ) : (
-                    <button>
+                    <button className="my-5">
                         <Link to='/login' className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-6 rounded'> 
                             Login
                         </Link>
