be3c2f038e13c7a2ad6a5b82e3b99f508e200ce6
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-04-26 21:07:13 +0200

    Frontend - Display connected user name.

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -17,6 +17,7 @@ const NavBar: React.FC = () => {
                 {user ? (
                     <>
                         <div className='flex flex-row gap-9'>
+                            <p className='desc'>{JSON.parse(user).name}</p>
                             <img src={JSON.parse(user).photo} alt="profile picture" className='w-12 rounded-full' />
                             <button onClick={logout} className='bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-6 rounded'> 
                                 Logout
