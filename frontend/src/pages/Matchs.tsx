eb4eb71f9671aa804f9d0cb1d2bba8b0b859b5b9
Hichem BOUZOURINE <hichembouzourine.uni@gmail.com>
2024-05-21 21:02:39 +0200

    Frontend - Add Search match feature.

--- frontend/src/pages/Matchs.tsx
+++ frontend/src/pages/Matchs.tsx
@@ -15,10 +15,13 @@ const Matchs = () => {
     const {user} = useAuth()
     const {socket, socketIsOpen, error, sendEvent} = useWebSocket()
     const [matchs, setMatchs] = useState<Match[]>([])
+    const [filteredMatchs, setFilteredMatchs] = useState<Match[]>([])
     const [chat, setChat] = useState<Chat | null>(null)
     const [selectedMatch, setSelectedMatch] = useState<Match | null>(null)
     const [loadingMatchs, setLoadingMatchs] = useState<boolean>(true)
     const [loadingChat, setLoadingChat] = useState<boolean>(false)
+    const [search, setSearch] = useState<string>("")
+
     const navigate = useNavigate()
 
     const onClickMatch = (chatId : string, match : Match) => {
@@ -33,6 +36,14 @@ const Matchs = () => {
         setLoadingChat(false)
     }
 
+    const searchMatchs = (search: string) => {
+        setSearch(search)
+        const desiredMatchs = matchs.filter((match) => {
+            return match.team1.toLowerCase().includes(search.toLowerCase()) || match.team2.toLowerCase().includes(search.toLowerCase())
+        })
+        setFilteredMatchs(desiredMatchs)
+    }
+
     useEffect(() => {
         if (!user) {
             navigate('/')
@@ -44,14 +55,34 @@ const Matchs = () => {
         setLoadingMatchs(false)
     }, [])
 
+    useEffect(() => {
+        // When the original matchs change, reset the filtered matchs
+        setFilteredMatchs(matchs)
+    }, [matchs])
+
     return (
         <div className={`flex flex-col items-center bg-gray-800 ${chat ? "h-full" : "h-screen"} lg:h-screen`}>
             <div className="w-screen">
                 <NavBar />
             </div>
-            <div className="orange_gradient text-5xl mb-8 md:text-xl lg:text-4xl">
+            
+            {!loadingMatchs && (
+                <div className="flex flex-row justify-center items-center gap-4  text-5xl mb-8 md:text-xl lg:text-4xl">
+                    <p className="text-sm md:text-3xl orange_gradient">Search for your team:</p>
+                    <input 
+                        type="search" 
+                        name="search" 
+                        id="search" 
+                        className="rounded-lg outline-none p-2 w-5/12 h-10 bg-slate-500" 
+                        value={search} 
+                        onChange={(e) => {searchMatchs(e.target.value)}} 
+                    />
+                </div>
+            )}
+
+            {/* <div className="orange_gradient text-5xl mb-8 md:text-xl lg:text-4xl">
                 Select a match and join the discussion
-            </div>
+            </div> */}
             <div className="w-5/6">
                 <div className="flex flex-col lg:flex-row gap-4">
                     {loadingChat && (
@@ -73,7 +104,7 @@ const Matchs = () => {
                                     <BallTriangle color="#ffffff" height={50} width={50} />
                                 </div>
                             )}
-                            {matchs.map((match, index) => (
+                            {filteredMatchs.map((match, index) => (
                                 <MatchElement match={match} key={index} onClickMatch={onClickMatch} selectedMatch={selectedMatch}/>
                             ))}
                         </div>
